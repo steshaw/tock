095b547f9ee20debb8bc3bda253fd3e3f03338c8
Neil Brown <neil@twistedsquare.com>
2007-08-28 12:43:30 +0000

    Added Haddock documentation to the TestMain module

--- TestMain.hs
+++ TestMain.hs
@@ -16,6 +16,15 @@ You should have received a copy of the GNU General Public License along
 with this program.  If not, see <http://www.gnu.org/licenses/>.
 -}
 
+-- | A module containing the 'main' function for the Tock test suite.  It currently runs tests from the following modules:
+--
+-- * "PassTest"
+--
+-- * "RainParseTest"
+--
+-- * "RainPassTest"
+--
+-- * "UsageCheckTest"
 module TestMain () where
 
 import qualified RainParseTest (tests)
