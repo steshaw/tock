341e3244153c961782f268a19f557975ff93fe9c
Neil Brown <neil@twistedsquare.com>
2007-11-02 16:46:42 +0000

    Added an export list to the RainUsageCheck module

--- transformations/RainUsageCheck.hs
+++ transformations/RainUsageCheck.hs
@@ -20,7 +20,7 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 -- the control-flow graph stuff, hence the use of functions that match the dictionary
 -- of functions in FlowGraph.  This is also why we don't drill down into processes;
 -- the control-flow graph means that we only need to concentrate on each node that isn't nested.
-module RainUsageCheck where
+module RainUsageCheck (Var(..), Vars(..), vars, Decl(..), getVarLabelFuncs, emptyVars, getVarProc, checkInitVar, findReachDef) where
 
 import Control.Monad.Error
 import Control.Monad.Identity
