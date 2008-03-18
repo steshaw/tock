95fd6321d1faa32d0e1212cb2ea0227fefa835df
Neil Brown <neil@twistedsquare.com>
2008-03-18 11:53:57 +0000

    Added support for parsing list types in Rain

--- frontends/ParseRain.hs
+++ frontends/ParseRain.hs
@@ -158,6 +158,7 @@ dataType
     <|> do {sChannel ; inner <- dataType ; return $ A.Chan A.DirUnknown (A.ChanAttributes {A.caWritingShared = False, A.caReadingShared = False}) inner}
     <|> do {sIn ; inner <- dataType ; return $ A.Chan A.DirInput (A.ChanAttributes {A.caWritingShared = False, A.caReadingShared = False}) inner}
     <|> do {sOut ; inner <- dataType ; return $ A.Chan A.DirOutput (A.ChanAttributes {A.caWritingShared = False, A.caReadingShared = False}) inner}
+    <|> do {sLeftQ ; inner <- dataType ; sRightQ ; return $ A.List inner}
     <|> do {(m,n) <- identifier ; return $ A.UserDataType A.Name {A.nameMeta = m, A.nameName = n, A.nameType = A.DataTypeName}}
     <?> "data type"
 
