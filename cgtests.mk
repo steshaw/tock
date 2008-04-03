f7f01a3333df1c6546c53d600eaed230a2aa0205
Neil Brown <neil@twistedsquare.com>
2008-04-03 11:25:30 +0000

    Added some rules to cgtests.mk for profiling compilation of the cgtests and automatically generating PNGs of the profiles

--- cgtests.mk
+++ cgtests.mk
@@ -18,3 +18,15 @@ run-all: $(addprefix run-,$(tests))
 
 run-cgtests/%: cgtests/%
 	cgtests/$*
+
+profile-all: $(addprefix profile-,$(tests))
+
+# On Debian/Ubuntu, you will need the "tth" and "netpbm" packages for ps2png
+profile-cgtests/%: cgtests/%.occ
+	./tock -vk --backend=$(BACKEND) --mode=compile cgtests/$*.occ +RTS -hc -p -s > /dev/null
+	mv tock.hp tock-$*.hp
+	mv tock.stat tock-$*.stat
+	mv tock.prof tock-$*.prof
+	hp2ps -c -s -g tock-$*.hp
+	ps2png tock-$*.ps tock-$*.png
+
