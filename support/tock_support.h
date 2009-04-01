91092c17ff4fca077c87f05a3c9b5e802d10ebd8
Neil Brown <neil@twistedsquare.com>
2009-04-01 18:59:10 +0000

    Added some missing static specifiers to some occam support functions

--- support/tock_support.h
+++ support/tock_support.h
@@ -440,54 +440,54 @@ MAKE_DUMB_REM(double)
 // FIXME: Again, all these should check.
 
 //{{{ float
-float occam_convert_int64_t_float_round (int64_t, const char *) occam_unused;
-float occam_convert_int64_t_float_round (int64_t v, const char *pos) {
+static float occam_convert_int64_t_float_round (int64_t, const char *) occam_unused;
+static float occam_convert_int64_t_float_round (int64_t v, const char *pos) {
 	return (float) v;
 }
 
-float occam_convert_int64_t_float_trunc (int64_t, const char *) occam_unused;
-float occam_convert_int64_t_float_trunc (int64_t v, const char *pos) {
+static float occam_convert_int64_t_float_trunc (int64_t, const char *) occam_unused;
+static float occam_convert_int64_t_float_trunc (int64_t v, const char *pos) {
 	return (float) v;
 }
 
-int64_t occam_convert_float_int64_t_round (float, const char *) occam_unused;
-int64_t occam_convert_float_int64_t_round (float v, const char *pos) {
+static int64_t occam_convert_float_int64_t_round (float, const char *) occam_unused;
+static int64_t occam_convert_float_int64_t_round (float v, const char *pos) {
 	return (int64_t) roundf (v);
 }
 
-int64_t occam_convert_float_int64_t_trunc (float, const char *) occam_unused;
-int64_t occam_convert_float_int64_t_trunc (float v, const char *pos) {
+static int64_t occam_convert_float_int64_t_trunc (float, const char *) occam_unused;
+static int64_t occam_convert_float_int64_t_trunc (float v, const char *pos) {
 	return (int64_t) truncf (v);
 }
 
-float occam_convert_double_float_round (double, const char *) occam_unused;
-float occam_convert_double_float_round (double v, const char *pos) {
+static float occam_convert_double_float_round (double, const char *) occam_unused;
+static float occam_convert_double_float_round (double v, const char *pos) {
 	return (float) v;
 }
 
-float occam_convert_double_float_trunc (double, const char *) occam_unused;
-float occam_convert_double_float_trunc (double v, const char *pos) {
+static float occam_convert_double_float_trunc (double, const char *) occam_unused;
+static float occam_convert_double_float_trunc (double v, const char *pos) {
 	return (float) v;
 }
 //}}}
 //{{{ double
-double occam_convert_int64_t_double_round (int64_t, const char *) occam_unused;
-double occam_convert_int64_t_double_round (int64_t v, const char *pos) {
+static double occam_convert_int64_t_double_round (int64_t, const char *) occam_unused;
+static double occam_convert_int64_t_double_round (int64_t v, const char *pos) {
 	return (double) v;
 }
 
-double occam_convert_int64_t_double_trunc (int64_t, const char *) occam_unused;
-double occam_convert_int64_t_double_trunc (int64_t v, const char *pos) {
+static double occam_convert_int64_t_double_trunc (int64_t, const char *) occam_unused;
+static double occam_convert_int64_t_double_trunc (int64_t v, const char *pos) {
 	return (double) v;
 }
 
-int64_t occam_convert_double_int64_t_round (double, const char *) occam_unused;
-int64_t occam_convert_double_int64_t_round (double v, const char *pos) {
+static int64_t occam_convert_double_int64_t_round (double, const char *) occam_unused;
+static int64_t occam_convert_double_int64_t_round (double v, const char *pos) {
 	return (int64_t) round (v);
 }
 
-int64_t occam_convert_double_int64_t_trunc (double, const char *) occam_unused;
-int64_t occam_convert_double_int64_t_trunc (double v, const char *pos) {
+static int64_t occam_convert_double_int64_t_trunc (double, const char *) occam_unused;
+static int64_t occam_convert_double_int64_t_trunc (double v, const char *pos) {
 	return (int64_t) trunc (v);
 }
 //}}}
